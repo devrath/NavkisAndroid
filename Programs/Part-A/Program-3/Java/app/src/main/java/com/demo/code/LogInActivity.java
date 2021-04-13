c2364f43d724f095fa353519a3bd685f75cb15dc
Devrath AD <devrathad@mplgaming.com>
2021-04-13 23:00:41 +0530

    v

--- Programs/Part-A/Program-3/Java/app/src/main/java/com/demo/code/LogInActivity.java
+++ Programs/Part-A/Program-3/Java/app/src/main/java/com/demo/code/LogInActivity.java
@@ -1,15 +1,78 @@
 package com.demo.code;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private String userName,password;
+
+    private EditText usrNameId;
+    private EditText pwdId;
+    private Button loginId;
+
+    private int attemptCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        findViewsForTheScreen();
+        getDataFromPreviousScreen();
+        setOnClickListener();
+    }
+
+    private void setOnClickListener() {
+
+
+        loginId.setOnClickListener(v -> {
+            attemptCounter++;
+            String loginUserName = usrNameId.getText().toString();
+            String loginPassword = pwdId.getText().toString();
+            if(attemptLogin(loginUserName,loginPassword)){
+                //Navigate to success activity
+                Intent intent = new Intent(LogInActivity.this, SuccessActivity.class);
+                startActivity(intent);
+            }else{
+                if(attemptCounter>=2){
+                    // Disable the Login Button
+                    loginId.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    private boolean attemptLogin(String loginUserName, String loginPassword) {
+
+        if(userName.equals(loginUserName) && password.equals(loginPassword)){
+            return true;
+        }else{
+            Toast.makeText(LogInActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    }
+
+    private void getDataFromPreviousScreen() {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            userName = bundle.getString("userName");
+            password = bundle.getString("password");
+        }
+    }
+
+    /**
+     * Find the objects for the screen
+     */
+    private void findViewsForTheScreen() {
+        usrNameId = findViewById(R.id.usrNameId);
+        pwdId = findViewById(R.id.pwdId);
+        loginId = findViewById(R.id.loginId);
     }
 
 }
--- Programs/Part-A/Program-3/Java/app/src/main/java/com/demo/code/SignUpActivity.java
+++ Programs/Part-A/Program-3/Java/app/src/main/java/com/demo/code/SignUpActivity.java
@@ -2,13 +2,88 @@ package com.demo.code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText usrNameId;
+    private EditText pwdId;
+    private Button signUpId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        findViewsForTheScreen();
+        setOnClickListener();
     }
+
+    private void setOnClickListener() {
+        signUpId.setOnClickListener(v -> {
+
+            String userName = usrNameId.getText().toString();
+            String inputPwd = pwdId.getText().toString();
+
+           if(validatePassword(inputPwd) && validateUserName(userName)){
+
+               Bundle bundle = new Bundle();
+               bundle.putString("userName",userName);
+               bundle.putString("password",inputPwd);
+               Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
+               intent.putExtras(bundle);
+               startActivity(intent);
+
+           }
+        });
+    }
+
+    /*
+      Password should contain uppercase and lowercase letters.
+      Password should contain letters and numbers.
+      Password should contain special characters.
+      Minimum length of the password (the default value is 8).*/
+    private boolean validatePassword(String inputPwd) {
+
+        String regexValidation = "^(?=.*[a-z])(?=."
+                                + "*[A-Z])(?=.*\\d)"
+                                + "(?=.*[-+_!@#$%^&*., ?]).+$";
+
+        Pattern p = Pattern.compile(regexValidation);
+        Matcher m = p.matcher(inputPwd);
+
+        if(m.matches() && inputPwd.length()>=8){
+            return true;
+        }else{
+            Toast.makeText(SignUpActivity.this,"Enter Valid Password",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    /**
+     * Check if the user has entered a user name
+     */
+    private boolean validateUserName(String userName) {
+        return userName.length() > 0;
+    }
+
+    /**
+     * Find the objects for the screen
+     */
+    private void findViewsForTheScreen() {
+        usrNameId = findViewById(R.id.usrNameId);
+        pwdId = findViewById(R.id.pwdId);
+        signUpId = findViewById(R.id.signUpId);
+    }
+
+
 }
