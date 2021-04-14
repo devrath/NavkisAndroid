d1c07c48c5102472bd1af9ad2e3f599f03562afe
Devrath AD <devrathad@mplgaming.com>
2021-04-14 06:36:17 +0530

    v

---
+++ Programs/Part-A/Program-5/Java/app/src/main/java/com/demo/code/MainActivity.java
@@ -0,0 +1,48 @@
+package com.demo.code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private Chronometer chronometer;
+    private Button startId,stopId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewsForTheScreen();
+        setOnClickListeners();
+        chronometer.setOnChronometerTickListener(chronometerChanged -> chronometer = chronometerChanged);
+    }
+
+    private void findViewsForTheScreen() {
+        chronometer = findViewById(R.id.chronometer);
+        startId = findViewById(R.id.startId);
+        stopId = findViewById(R.id.stopId);
+    }
+
+    private void setOnClickListeners() {
+        startId.setOnClickListener(v -> {
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            chronometer.start();
+        });
+        stopId.setOnClickListener(v -> {
+            chronometer.stop();
+        });
+    }
+
+}
