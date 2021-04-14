e89232e2070d8ae5a0f5432af2de36f5495decfa
Devrath AD <devrathad@mplgaming.com>
2021-04-14 11:14:01 +0530

    k

--- Programs/Part-A/Program-2/Kotlin/app/src/main/java/com/demo/code/MainActivity.kt
+++ Programs/Part-A/Program-2/Kotlin/app/src/main/java/com/demo/code/MainActivity.kt
@@ -5,26 +5,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.demo.code.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var inputFieldId: EditText
-    lateinit var numZeroId: Button
-    lateinit var numOneId: Button
-    lateinit var numTwoId: Button
-    lateinit var numThreeId: Button
-    lateinit var numFourId: Button
-    lateinit var numFiveId: Button
-    lateinit var numSixId: Button
-    lateinit var numSevenId: Button
-    lateinit var numEightId: Button
-    lateinit var numNineId: Button
-    lateinit var plusId: Button
-    lateinit var minusId: Button
-    lateinit var multiplicationId: Button
-    lateinit var divisionId: Button
-    lateinit var equalsId: Button
-    lateinit var dotId: Button
-    lateinit var clearId: Button
 
     var leftHandSide : Double = 0.0
     var rightHandSide : Double = 0.0
@@ -35,148 +18,62 @@ class MainActivity : AppCompatActivity() {
 
     private var currentOperation: Operation? = null
 
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewsForTheScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setOnclickListeners()
     }
 
-    private fun findViewsForTheScreen() {
-        // Numbers
-        inputFieldId = findViewById(R.id.inputFieldId)
-        numZeroId = findViewById(R.id.numZeroId)
-        numOneId = findViewById(R.id.numOneId)
-        numTwoId = findViewById(R.id.numTwoId)
-        numThreeId = findViewById(R.id.numThreeId)
-        numFourId = findViewById(R.id.numFourId)
-        numFiveId = findViewById(R.id.numFiveId)
-        numSixId = findViewById(R.id.numSixId)
-        numSevenId = findViewById(R.id.numSevenId)
-        numEightId = findViewById(R.id.numEightId)
-        numNineId = findViewById(R.id.numNineId)
-        // Operation
-        plusId = findViewById(R.id.plusId)
-        minusId = findViewById(R.id.minusId)
-        multiplicationId = findViewById(R.id.multiplicationId)
-        divisionId = findViewById(R.id.divisionId)
-        equalsId = findViewById(R.id.equalsId)
-        dotId = findViewById(R.id.dotId)
-        clearId = findViewById(R.id.clearId)
-    }
-
     private fun setOnclickListeners() {
-        plusId.setOnClickListener {
-            // Operator Plus Field
-            selectOperation(Operation.ADD)
-        }
-        minusId.setOnClickListener {
-            // Operator Minus Field
-            selectOperation(Operation.SUBTRACT)
-        }
-        multiplicationId.setOnClickListener {
-            // Operator Multiplication Field
-            selectOperation(Operation.MULTIPLY)
-        }
-        divisionId.setOnClickListener {
-            // Operator Division Field
-            selectOperation(Operation.DIVIDE)
-        }
-        equalsId.setOnClickListener {
-            // Operator Equals Field
-            performEvaluation()
-        }
-        dotId.setOnClickListener {
-            // Operator Dot Field
-            inputFieldId.append(".")
-        }
-        clearId.setOnClickListener {
-            // Operator Clear Field
-            resetCalculator()
-        }
-        numZeroId.setOnClickListener {
-            // Button Number Zero
-            inputFieldId.append("0")
-        }
-        numOneId.setOnClickListener {
-            // Button Number One
-            inputFieldId.append("1")
-        }
-        numTwoId.setOnClickListener {
-            // Button Number Two
-            inputFieldId.append("2")
-        }
-        numThreeId.setOnClickListener {
-            // Button Number Three
-            inputFieldId.append("3")
-        }
-        numFourId.setOnClickListener {
-            // Button Number Four
-            inputFieldId.append("4")
-        }
-        numFiveId.setOnClickListener {
-            // Button Number Five
-            inputFieldId.append("5")
-        }
-        numSixId.setOnClickListener {
-            // Button Number Six
-            inputFieldId.append("6")
-        }
-        numSevenId.setOnClickListener {
-            // Button Number Seven
-            inputFieldId.append("7")
-        }
-        numEightId.setOnClickListener {
-            // Button Number Eight
-            inputFieldId.append("8")
-        }
-        numNineId.setOnClickListener {
-            // Button Number Nine
-            inputFieldId.append("9")
+        binding.apply {
+            plusId.setOnClickListener { selectOperation(Operation.ADD) }
+            minusId.setOnClickListener { selectOperation(Operation.SUBTRACT) }
+            multiplicationId.setOnClickListener { selectOperation(Operation.MULTIPLY) }
+            divisionId.setOnClickListener { selectOperation(Operation.DIVIDE) }
+            equalsId.setOnClickListener { performEvaluation() }
+            dotId.setOnClickListener { inputFieldId.append(".") }
+            clearId.setOnClickListener { resetCalculator() }
+            numZeroId.setOnClickListener { inputFieldId.append("0") }
+            numOneId.setOnClickListener { inputFieldId.append("1") }
+            numTwoId.setOnClickListener { inputFieldId.append("2") }
+            numThreeId.setOnClickListener { inputFieldId.append("3") }
+            numFourId.setOnClickListener { inputFieldId.append("4") }
+            numFiveId.setOnClickListener { inputFieldId.append("5") }
+            numSixId.setOnClickListener { inputFieldId.append("6") }
+            numSevenId.setOnClickListener { inputFieldId.append("7") }
+            numEightId.setOnClickListener { inputFieldId.append("8") }
+            numNineId.setOnClickListener { inputFieldId.append("9") }
         }
     }
 
     private fun resetCalculator() {
-        inputFieldId.setText("")
+        binding.inputFieldId.setText("")
         leftHandSide = 0.0
         rightHandSide = 0.0
     }
 
     private fun performEvaluation() {
-
-        // Set the value for left hand side of the expression
-        val input = inputFieldId.text.toString()
+        val input = binding.inputFieldId.text.toString()
         rightHandSide = input.toDouble()
         var result = 0.0
-        if (currentOperation == Operation.ADD) {
-            // Addition
-            result = leftHandSide + rightHandSide
-        } else if (currentOperation == Operation.SUBTRACT) {
-            // Subtraction
-            result = leftHandSide - rightHandSide
-        } else if (currentOperation == Operation.MULTIPLY) {
-            // Multiplication
-            result = leftHandSide * rightHandSide
-        } else if (currentOperation == Operation.DIVIDE) {
-            // DIVISION
-            result = leftHandSide / rightHandSide
+        when (currentOperation) {
+            Operation.ADD -> {  result = leftHandSide + rightHandSide }
+            Operation.SUBTRACT -> { result = leftHandSide - rightHandSide }
+            Operation.MULTIPLY -> { result = leftHandSide * rightHandSide }
+            Operation.DIVIDE -> { result = leftHandSide / rightHandSide }
         }
-        inputFieldId.setText("")
-        inputFieldId.setText(result.toString())
+        binding.inputFieldId.setText("")
+        binding.inputFieldId.setText(result.toString())
     }
 
     private fun selectOperation(operation: Operation) {
-        // Set the operation
         currentOperation = operation
-
-        // Set the value for left hand side of the expression
-        val input = inputFieldId.text.toString()
-        input?.let {
-
-        }
+        val input = binding.inputFieldId.text.toString()
         leftHandSide = input.toDouble()
-
-        // Clear the input field so user can enter the right hand side of expression
-        inputFieldId.setText("")
+        binding.inputFieldId.setText("")
     }
 }
